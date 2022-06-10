ab89b1efc5664ece4480f26ff926cba0ea02461e
Javier Granados <hola@javiergranados.dev>
2022-06-10 12:32:57 +0200

    Install and implement the library react-native-vector-icons

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const MapScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Map screen</Text>
+      <Text style={styles.title}>
+        <Icon name="map-outline" color="grey" size={50} />
+        Map screen
+      </Text>
     </View>
   );
 };
