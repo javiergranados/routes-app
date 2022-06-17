37335165c3284b78074d9c196663a7d1056187f6
Javier Granados <hola@javiergranados.dev>
2022-06-17 11:34:57 +0200

    Add a Marker for tests

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-// import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import MapView from 'react-native-maps';
+import { Map } from '../components/Map';
 
 const MapScreen = () => {
   return (
     <View style={styles.container}>
-      <MapView
-        style={{ flex: 1 }}
-        // provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+      <Map />
     </View>
   );
 };
